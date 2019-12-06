5758aeba2a137f61fabff0138291bce40097cb84
j97b <jamesbarron97@gmail.com>
2019-12-06 09:20:51 +0000

    first commit

---
+++ src/main/java/com/bae/javaAssessment/App.java
@@ -0,0 +1,10 @@
+package com.bae.javaAssessment;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/bae/javaAssessment/javaAssessmentTests.java
@@ -0,0 +1,14 @@
+package com.bae.javaAssessment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class javaAssessmentTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
